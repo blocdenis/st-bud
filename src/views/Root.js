8b70182523a9d5f03b904df4127e176dab013f95
denis <denis@macbook-air-denis.home>
2023-01-30 09:09:22 +0100

    added some style on my userslist

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,17 @@
-import UsersList from 'components/UsersList/UsersList';
-import React from 'react';
+import React from "react";
+import UsersList from "components/organisms/UsersList/UsersList";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "assets/styles/GlobalStyle";
+import { theme } from "assets/styles/theme";
+import { Wrapper } from "./Root.styles";
 
 const Root = () => (
-   <>
-    <UsersList />
-  </>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Wrapper>
+      <UsersList />
+    </Wrapper>
+  </ThemeProvider>
 );
 
 export default Root;
